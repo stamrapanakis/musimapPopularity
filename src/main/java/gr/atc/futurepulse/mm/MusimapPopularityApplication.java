62140a0e295e27ed97c6911fa721f940179da4ce
Stamatis Rapanakis <s.rapanakis@atc.gr>
2020-01-16 10:37:40 +0200

    Basic structure

---
+++ src/main/java/gr/atc/futurepulse/mm/MusimapPopularityApplication.java
@@ -0,0 +1,13 @@
+package gr.atc.futurepulse.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusimapPopularityApplication {
+
+	public static void main(String[] args) {
+            SpringApplication.run(MusimapPopularityApplication.class, args);
+	}
+
+}
